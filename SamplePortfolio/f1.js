4722f697ea4817f60bc96b778e2f09ddad0260d8
akileshpanicker <48186557+akileshpanicker@users.noreply.github.com>
2020-04-12 15:28:45 +0530

    Add files via upload

---
+++ SamplePortfolio/f1.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  console.log("Hello")
+  mainNav.classList.toggle("active");
+});
+}
+
+
+
+
+
